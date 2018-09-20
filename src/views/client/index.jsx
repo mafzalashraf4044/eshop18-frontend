ed918505b36e0daed55f15ccdfc21023d6e815f5
Afzal Ashraf <afzalashraf.dev@gmail.com>
2018-09-20 18:05:25 +0500

    talk.to initialized after dom load

--- src/views/client/index.jsx
+++ src/views/client/index.jsx
@@ -89,6 +89,18 @@ class Client extends React.PureComponent {
           throw new Error(err);
         });
       }
+
+      var Tawk_API = Tawk_API || {},
+      Tawk_LoadStart = new Date();
+      (function () {
+        var s1 = document.createElement("script"),
+          s0 = document.getElementsByTagName("script")[0];
+        s1.async = true;
+        s1.src = 'https://embed.tawk.to/5ba1f1c3c666d426648ae5d0/default';
+        s1.charset = 'UTF-8';
+        s1.setAttribute('crossorigin', '*');
+        s0.parentNode.insertBefore(s1, s0);
+      })();
     });
   }
 
